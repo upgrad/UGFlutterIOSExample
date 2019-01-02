b0d797b73063914ebaf2880071549a8d7d11e3ba
kerry <me@prateekgrover.com>
2019-01-02 12:06:16 +0530

    Initial Commit

---
+++ UGFlutterIOSExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UGFlutterIOSExample
+//
+//  Created by Prateek Grover on 02/01/19.
+//  Copyright © 2019 UpGrad Education Private Limited. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
